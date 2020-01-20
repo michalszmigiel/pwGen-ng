3d5ff5d004cc4a9dcfb9416bcb52fcc5c5d34307
Michał R. Szmigiel <michal.r.szmigiel@gmail.com>
2020-01-20 11:32:23 +0100

    styling

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+https://pw.michalrszmigiel.now.sh/
+
 # Pw
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.14.
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw';
+  password = '';
+  pwLength = 0;
+  includeLetters = false;
+  includeNums = false;
+  includeSymbols = false;
+
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value);
+
+    if (!isNaN(parsedValue)) {
+      this.pwLength = parsedValue;
+    }
+  }
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+  onChangeUseNumbers() {
+    this.includeNums = !this.includeNums;
+  }
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+  onButtonClick() {
+    console.log(`
+    length: ${this.pwLength},
+    letters: ${this.includeLetters},
+    numbers: ${this.includeNums},
+    symbols: ${this.includeSymbols}`);
+    const numbers = '1234567890';
+    const letters = 'qwertyuiopasdfghjklzxcvbnm';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNums) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPw = '';
+
+    for (let i = 0; i < this.pwLength; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPw += validChars[index];
+    }
+
+    this.password = generatedPw;
+  }
 }
